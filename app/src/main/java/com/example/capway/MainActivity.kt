418080fea1d4d4436f3edc8e15b7c0b5422be160
Matt Shaw <msspshaw@gmail.com>
2022-05-11 13:52:28 -0700

    Adding search bar

--- app/src/main/java/com/example/capway/MainActivity.kt
+++ app/src/main/java/com/example/capway/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : FragmentActivity(), View.OnClickListener {
